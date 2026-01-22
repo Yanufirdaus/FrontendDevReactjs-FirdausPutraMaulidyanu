17abdd96e63c958985b0a94d02fa627d00ef2b55
Firdaus Putra Maulidyanu <94530597+Yanufirdaus@users.noreply.github.com>
2026-01-23 04:56:31 +0700

    Refactor restaurant data fetching in DetailResto

--- src/pages/DetailResto.jsx
+++ src/pages/DetailResto.jsx
@@ -10,37 +10,34 @@ const DetailResto = () => {
   const [restaurant, setRestaurant] = useState(null);
 
   useEffect(() => {
-    async function load() {
-        await fetchRestaurantDetails(); // ambil data di sini
-    }
-    load();
-    }, []);
+    const fetchRestaurantDetails = async () => {
+      try {
+        const response = await axios.get(
+          `https://646ce8a07b42c06c3b2c4013.mockapi.io/restaurant/${id}`
+        );
+        setRestaurant(response.data);
+      } catch (error) {
+        console.error("Failed to fetch restaurant details", error);
+      }
+    };
 
-  const fetchRestaurantDetails = async () => {
-    try {
-      const response = await axios.get(
-        `https://646ce8a07b42c06c3b2c4013.mockapi.io/restaurant/${id}`
-      );
-      setRestaurant(response.data);
-    } catch (error) {
-      console.error("Failed to fetch restaurant details", error);
-    }
-  };
-    if (!restaurant) {
-        return <div className="fixed-grid has-4-cols has-2-cols-mobile has-background-white has-text-centered p-6">
-            <Riple color="#242424" size="medium" text="" textColor="" />
-        </div>
-    }
-        return (
-            <section>
-                <HeaderDashboard />
-                <DetailPage 
-                    restaurant={restaurant} 
-                    currentId={id}
-                />
-            </section>
-            
+    fetchRestaurantDetails();
+  }, [id]);
+  
+  if (!restaurant) {
+    return (
+      <div className="fixed-grid has-4-cols has-2-cols-mobile has-background-white has-text-centered p-6">
+        <Riple color="#242424" size="medium" text="" textColor="" />
+      </div>
     );
-}
+  }
+
+  return (
+    <section>
+      <HeaderDashboard />
+      <DetailPage restaurant={restaurant} currentId={id} />
+    </section>
+  );
+};
 
 export default DetailResto;
